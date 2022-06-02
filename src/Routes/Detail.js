ab266b5393faf912fc782c459f13242b57d873cf
junseokoh-hub <lunulatecat@naver.com>
2022-06-02 15:48:18 +0900

    call serveral data

--- src/Routes/Detail.js
+++ src/Routes/Detail.js
@@ -1,10 +1,20 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { API_KEY, API_URL } from "../Config";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from "../Config";
+import styled from "styled-components";
+
+const SeparatePoster = styled.img`
+  display: block;
+  margin: 1em 0 0 1em;
+  width: 15em;
+`;
 
 function Detail({ movie }) {
+  const [data, setData] = useState({});
+  const [showData, setShowData] = useState({});
   const { id } = useParams();
-  const getMovie = useCallback(async () => {
+
+  const fetchMedia = useCallback(async () => {
     const response = await fetch(
       `https://${API_URL}${
         movie ? "movie" : "tv"
@@ -12,12 +22,60 @@ function Detail({ movie }) {
     );
     const json = await response.json();
     console.log(json);
+    setData(json);
   }, [id, movie]);
-  useEffect(() => {
-    getMovie();
-  }, [getMovie]);
 
-  return <>{movie ? <div>Movie Detail</div> : <div>TV Detail</div>}</>;
+  const fetchMediaVideos = useCallback(async () => {
+    const response = await fetch(
+      `https://${API_URL}${
+        movie ? "movie" : "tv"
+      }/${id}/videos?api_key=${API_KEY}&language=en-US`,
+    );
+    const json = await response.json();
+    console.log(json);
+    setShowData(json);
+  }, [id, movie]);
+
+  useEffect(() => {
+    fetchMedia();
+    fetchMediaVideos();
+  }, [fetchMedia, fetchMediaVideos]);
+
+  return (
+    <>
+      <div>
+        {data && (
+          <SeparatePoster
+            src={`https://${IMAGE_BASE_URL}/w200${data.poster_path}`}
+            alt={data.title}
+          />
+        )}
+        <div style={{ width: "15em", marginLeft: "1em" }}>
+          {data.genres &&
+            data.genres.map((item) => {
+              return (
+                <span
+                  style={{
+                    display: "inline-block",
+                    margin: "0 0.5em",
+                  }}
+                >
+                  {item.name}
+                </span>
+              );
+            })}
+        </div>
+        {data && <p>{data.homepage}</p>}
+        {showData.results && (
+          <iframe
+            style={{ width: "30em", height: "25em" }}
+            title={showData.results[0].id}
+            src={`https://www.youtube.com/embed/${showData.results[0].key}?autoplay=1`}
+          ></iframe>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Detail;
