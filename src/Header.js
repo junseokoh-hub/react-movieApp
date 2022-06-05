1b1d21908e8bbce50ce089e174034ddceeb9ed3d
junseokoh-hub <lunulatecat@naver.com>
2022-06-05 17:18:06 +0900

    add simple search bar on the header

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
@@ -16,6 +16,12 @@ const Header = styled.header`
     padding: 0 0.5em;
     cursor: pointer;
   }
+  .disappear {
+    display: none;
+  }
+  .appear {
+    display: inline-block;
+  }
 `;
 
 const Ul = styled.ul`
@@ -24,28 +30,54 @@ const Ul = styled.ul`
   justify-content: space-between;
   list-style: none;
   padding: 0 1em;
+  input {
+    border: 1px solid #fff;
+    padding: 0.2em 0.3em;
+    outline: none;
+    background-color: ${(props) => props.theme.bgColor};
+    &::placeholder {
+      color: ${(props) => props.theme.whiteColor};
+    }
+  }
+`;
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
 `;
 
 function Head() {
+  const [open, setOpen] = useState(false);
+
+  const openSearch = () => {
+    setOpen((open) => !open);
+  };
+
   return (
     <Header>
       <Ul>
-        <li>
+        <Li>
           <Link to="/">
             <span>Movies</span>
           </Link>
           <Link to="/tv">
             <span>TV</span>
           </Link>
-        </li>
-        <li>
+        </Li>
+        <Li>
+          <span onClick={openSearch}>🔍</span>
+          <input
+            className={!open ? "disappear" : "appear"}
+            type="text"
+            placeholder="Search..."
+          />
           <Link to="/search">
             <span>Search</span>
           </Link>
           <Link to="/myPage">
             <span>My Page</span>
           </Link>
-        </li>
+        </Li>
       </Ul>
     </Header>
   );
